db882d19d0a18b339b823c20fdd376b739e9f1f5
theclubfoot <papakwesiadams@icloud.com>
2022-03-30 15:33:20 -0700

    Header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int -strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _is_prime_number(int n);
+int _is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
